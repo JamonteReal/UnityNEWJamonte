9f01ef761dbf6cf0e766e56eec534d0af2302068
Student Jamie Samonte (TL_KCOMP_D_Y1) <t00302737@mymtu.ie>
2026-09-23 11:50:41 +0100

    I added movement to the bullet and ensured that it moves at the set speed regardless of FPS

--- UnityNEWJamonte/Assets/BulletScript.cs
+++ UnityNEWJamonte/Assets/BulletScript.cs
@@ -2,15 +2,12 @@ using UnityEngine;
 
 public class BulletScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    // This is the speed of the bullet
+    private float bulletSpeed = 20f;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // This implements the movement of the bullet - It'll move forward at the same speed regardless of FPS
+        transform.position += bulletSpeed * transform.forward * Time.delta;
     }
 }
